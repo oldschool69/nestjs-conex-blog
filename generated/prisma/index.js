ea39476cee38d1b57e723bd6bb86f3d8a51d39b5
Oldschool69 <flavio.marcondes@gmail.com>
2025-05-19 21:00:34 -0300

    add prisma client

--- generated/prisma/index.js
+++ generated/prisma/index.js
@@ -154,7 +154,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "../../prisma",
@@ -164,6 +164,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": true,
   "inlineDatasources": {
     "db": {
       "url": {
