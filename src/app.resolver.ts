f89ba08030bbd0c364e9ff32c9e3f5c402f39d83
Oldschool69 <flavio.marcondes@gmail.com>
2025-05-19 23:05:41 -0300

    fixing app errors

--- src/app.resolver.ts
+++ src/app.resolver.ts
@@ -4,6 +4,6 @@ import { Query, Resolver } from '@nestjs/graphql'
 export class AppResolver {
     @Query(() => String)
     hello(): string {
-        return 'Ola dev!'
+        return 'Ola dear dev!'
     }
 }
