f1274ec26e3cc100b16670d777627ceea9717fe6
Oldschool69 <flavio.marcondes@gmail.com>
2025-06-02 22:30:34 -0300

    implementando authors resolver

--- src/authors/graphql/resolvers/authors.resolver.ts
+++ src/authors/graphql/resolvers/authors.resolver.ts
@@ -1,14 +1,25 @@
-import { PrismaService } from '@/database/prisma/prisma.service';
-import { Query, Resolver } from '@nestjs/graphql';
-import { Author } from '../models/author';
+import { Args, Query, Resolver } from '@nestjs/graphql'
+import { Author } from '../models/author'
+import { Inject } from '@nestjs/common'
+import { ListAuthorsUsecase } from '@/authors/usercases/list-authors.usecase'
+import { SearchParamsArgs } from '../args/search-params.args'
+import { SearchAuthorsResult } from '../models/search-authors-result'
 
 @Resolver(() => Author)
 export class AuthorsResolver {
-    constructor(private prisma: PrismaService) {}
+    @Inject(ListAuthorsUsecase.Usecase)
+    private listAuthorsUseCase: ListAuthorsUsecase.Usecase
 
-    @Query(() => [Author])
-    authors() {
-        return this.prisma.author.findMany()
+    @Query(() => SearchAuthorsResult)
+    authors(
+        @Args() { page, perPage, sort, sortDir, filter }: SearchParamsArgs,
+    ) {
+        return this.listAuthorsUseCase.execute({
+            page,
+            perPage,
+            sort,
+            sortDir,
+            filter,
+        })
     }
-
 }
