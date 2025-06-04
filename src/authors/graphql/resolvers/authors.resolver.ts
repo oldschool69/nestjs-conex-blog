3c72024a7837031993c29a1171c155b7efb7658a
Oldschool69 <flavio.marcondes@gmail.com>
2025-06-03 21:06:14 -0300

    Criar mutation para adicionar author

--- src/authors/graphql/resolvers/authors.resolver.ts
+++ src/authors/graphql/resolvers/authors.resolver.ts
@@ -1,14 +1,19 @@
-import { Args, Query, Resolver } from '@nestjs/graphql'
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
 import { Author } from '../models/author'
 import { Inject } from '@nestjs/common'
 import { ListAuthorsUsecase } from '@/authors/usercases/list-authors.usecase'
 import { SearchParamsArgs } from '../args/search-params.args'
 import { SearchAuthorsResult } from '../models/search-authors-result'
+import { CreateAuthorUsecase } from '@/authors/usercases/create-author.usecase'
+import { CreateAuthorInput } from '../inputs/create-author.input'
 
 @Resolver(() => Author)
 export class AuthorsResolver {
     @Inject(ListAuthorsUsecase.Usecase)
     private listAuthorsUseCase: ListAuthorsUsecase.Usecase
+
+    @Inject(CreateAuthorUsecase.Usecase)
+    private createAuthorUseCase: CreateAuthorUsecase.Usecase
 
     @Query(() => SearchAuthorsResult)
     authors(
@@ -21,5 +26,10 @@ export class AuthorsResolver {
             sortDir,
             filter,
         })
+    }
+
+    @Mutation(() => Author)
+    createAuthor(@Args('data') data: CreateAuthorInput) {
+        return this.createAuthorUseCase.execute(data)
     }
 }
