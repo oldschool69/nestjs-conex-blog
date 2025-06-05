f527d8bc345abdafec269d7312740bfc1be9bbc0
Oldschool69 <flavio.marcondes@gmail.com>
2025-06-04 21:58:06 -0300

    adicionada validação

--- src/authors/graphql/inputs/create-author.input.ts
+++ src/authors/graphql/inputs/create-author.input.ts
@@ -1,11 +1,16 @@
-import { tr } from '@faker-js/faker'
 import { Field, InputType } from '@nestjs/graphql'
+import { IsEmail, IsNotEmpty, isNotEmpty, IsString } from 'class-validator'
 
 @InputType()
 export class CreateAuthorInput {
+    @IsString()
+    @IsNotEmpty()
     @Field()
     name: string
 
+    @IsString()
+    @IsEmail()
+    @IsNotEmpty()
     @Field()
     email: string
 }
